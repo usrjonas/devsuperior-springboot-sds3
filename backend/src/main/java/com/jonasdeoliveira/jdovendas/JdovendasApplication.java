215726718a9b63f11404f4197cea56960df5708c
Jonas de Oliveira <jonas.oliveira1402@outlook.com>
2021-05-04 05:03:17 -0300

    Project created

---
+++ backend/src/main/java/com/jonasdeoliveira/jdovendas/JdovendasApplication.java
@@ -0,0 +1,13 @@
+package com.jonasdeoliveira.jdovendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdovendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdovendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jonasdeoliveira/jdovendas/JdovendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonasdeoliveira.jdovendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdovendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
